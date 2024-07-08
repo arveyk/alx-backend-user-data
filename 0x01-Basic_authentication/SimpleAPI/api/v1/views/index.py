7456f824b643053cd97e5fbcc4a527fa5d4b72c3
Harvey W. Kis <harveykisiangani@gmail.com>
2024-07-08 16:21:10 +0300

    Forbidden and Unauthorized Error message

--- 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
@@ -24,3 +24,19 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    Return: Unauthorized error message
+    """
+    abort(401, description="Unauthorized")
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return: Forbidden error message
+    """
+    abort(403)
+    # description="Unauthorized")
