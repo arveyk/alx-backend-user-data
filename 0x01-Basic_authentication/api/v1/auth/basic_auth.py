b6eddb248d75290b3009138b190492625ecb39ae
Harvey W. Kis <harveykisiangani@gmail.com>
2024-07-09 16:54:55 +0300

    basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """Authentication module
 """
+from api.v1.auth.auth import Auth
 from flask import request
 from typing import TypeVar, List
-from api.v1.auth.auth import Auth
+
 
 class BasicAuth(Auth):
     """Basic Auth class
