b83d1392390ed85bbfc9186ca3e12354b9d4c0fa
Harvey W. Kis <harveykisiangani@gmail.com>
2024-07-09 17:39:07 +0300

    Basic: Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,21 @@ from typing import TypeVar, List
 class BasicAuth(Auth):
     """Basic Auth class
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+
+        """extraction of auth or something
+        Args:
+            authrization_header: header to use
+        Returns: authorozation header or None"""
+        if authorization_header is None:
+            return None
+        if isinstance(authorization_header, str) is False:
+            return None
+        if len(authorization_header) >= 7:
+            Basic = authorization_header[:6]
+
+            if Basic == 'Basic ':
+                return authorization_header[6:]
+            return None
+        return None
