d0ae6d65378e568eedf2c516abdbb35f084e063e
Harvey W. Kis <harveykisiangani@gmail.com>
2024-07-13 18:03:34 +0300

    Current user retrived

--- 0x01-Basic_authentication/main_6.py
+++ 0x01-Basic_authentication/main_6.py
@@ -15,5 +15,5 @@ user.password = user_clear_pwd
 print("New user: {} / {}".format(user.id, user.display_name()))
 user.save()
 
-basic_clear = "{}:{}".format(user_email, user_clear_pwd())
+basic_clear = "{}:{}".format(user_email, user_clear_pwd)
 print("Basic Base64: {}".format(base64.b64encode(basic_clear.encode('utf-8')).decode("utf-8")))
