32ecbb5221db73bd6f4affa5b660f300a9843bc1
Harvey W. Kis <harveykisiangani@gmail.com>
2024-07-13 13:03:09 +0300

    Access request attribute headers

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -46,10 +46,9 @@ class Auth:
         """
         if request is None:
             return None
-        # if request.get("Authentication"):
-        #    return None
-        # return request.get("Authentication")
-        return request.headers
+        if request.headers.get("Authorization") is None:
+            return None
+        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Check the current user
