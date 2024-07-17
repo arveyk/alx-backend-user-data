a477c7116bb23bb155125619fa13a418d7e99a96
Harvey W. Kis <harveykisiangani@gmail.com>
2024-07-17 17:22:13 +0300

    Find user

--- 0x03-user_authentication_service/2-main.py
+++ 0x03-user_authentication_service/2-main.py
@@ -13,17 +13,17 @@ my_db = DB()
 user = my_db.add_user("test@test.com", "PwdHashed")
 print(user.id)
 
-find_user = my_db.find_user(email="test@test.com")
+find_user = my_db.find_user_by(email="test@test.com")
 print(find_user.id)
 
 try:
     find_user = my_db.find_user_by(email="test2@test.com")
     print(find_user.id)
-except ValueError:
+except NoResultFound:
     print("Not found")
 
 try:
     find_user = my_db.find_user_by(no_email="test2@test.com")
     print(find_user.id)
-except ValueError:
+except InvalidRequestError:
     print("Invalid")
--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 """DB Module
 """
+from typing import TypeVar
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
-from user import Base
+from user import Base, User
+
 
 class DB:
     """DB class
@@ -15,7 +19,7 @@ class DB:
     def __init__(self) -> None:
         """ Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -28,3 +32,46 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+        """Adds a user to database
+        Args:
+            email: user email
+            hashed_password: users hashed password
+        Returns: User object"""
+        person = User(email=email, hashed_password=hashed_password)
+        session = self._session
+        session.add(person)
+        session.commit()
+        return person
+
+    def find_user_by(self, **kwarg):
+        """Find user in database
+        Args:
+            kwargs: keyword arguments
+        Returns: first row found in users"""
+        try:
+            result = ""
+            key1 = list(kwarg.values())
+            #result = self.__session.query(User).filter_by(email=key1[0]).one()
+            result = self.__session.query(User).filter(User.email == key1[0]).one()
+            return result
+        except NoResultFound as err:
+            raise(err)
+        except InvalidRequestError as invalid:
+            raise(invalid)
+        #query and filter by input argument
+        #NoResultFound
+        #InvalidRequestError
+
+    def update_user(user_id: int, **kwargs) -> None:
+        """Updates user
+        Args:
+        Returns:
+        """
+        #user = self.find_user_by(user_id)
+        # update user 
+        # commit changes
+        #if value not found in user:
+        #    raise ValueError
+        pass
