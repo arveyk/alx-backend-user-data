733352527688ec24f1aef3cf800541a449ac4ac7
Harvey W. Kis <harveykisiangani@gmail.com>
2024-07-18 13:53:43 +0300

    Basic flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,13 +2,30 @@
 """
 App module
 """
-from flask import Flask
+import requests
+from auth import Auth
+from flask import Flask, jsonify
+
+AUTH = AUTH()
 
 app = Flask(__name__)
 
-@app(route='/')
-def methods=['GET']:
-    return flask.jasonify()
+
+@app.route('/', methods=['GET'])
+def home():
+    msg = {"message": "Bienvenue"}
+    return jsonify(msg)
+
+@app.route('/users', methods['POST'])
+def users(requests):
+    """Registers users
+    """
+    # search for user
+    data = {"email" email, "password": password}
+    if user does not exist
+    return jsonify({"message": "email already registered"}), 400
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
