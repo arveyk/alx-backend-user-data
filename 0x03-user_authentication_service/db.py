923d5fbe531c0be9f20e97c3ef7113849df2ad8e
Harvey W. Kis <harveykisiangani@gmail.com>
2024-07-19 20:25:26 +0300

    Return Class

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -33,7 +33,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """Adds a user to database
         Args:
             email: user email
