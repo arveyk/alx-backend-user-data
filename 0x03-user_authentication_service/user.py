0ec7fce76eef8df5657bdca918cc25a259e2ab8b
Harvey W. Kis <harveykisiangani@gmail.com>
2024-07-19 20:10:39 +0300

    Sequence import removed

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """User Module
 """
-from sqlalchemy import Column, Integer, String, Sequence
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
